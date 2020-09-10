7bc8918b1b3eec4bb7894fb27dd74f59573a7521
Guolin Ke <guolin.ke@outlook.com>
2020-09-10 08:21:45 +0800

    Update regression_objective.hpp

--- src/objective/regression_objective.hpp
+++ src/objective/regression_objective.hpp
@@ -172,14 +172,12 @@ class RegressionL2loss: public ObjectiveFunction {
     double suml = 0.0f;
     double sumw = 0.0f;
     if (weights_ != nullptr) {
-      #pragma omp parallel for schedule(static) reduction(+:suml, sumw)
       for (data_size_t i = 0; i < num_data_; ++i) {
         suml += label_[i] * weights_[i];
         sumw += weights_[i];
       }
     } else {
       sumw = static_cast<double>(num_data_);
-      #pragma omp parallel for schedule(static) reduction(+:suml)
       for (data_size_t i = 0; i < num_data_; ++i) {
         suml += label_[i];
       }
